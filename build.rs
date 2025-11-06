44510b22f76c9dece170c9dcbb0273afe7150fcb
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-11-06 17:51:46 +0100

    feat(build): auto find proto files
    Previously, proto interface implementers had to list all .proto files by
    name in the build.rs file.
    
    This commits scripts this process using the walkdir crate.
    This crate is used for the code to appear cleaner.. But it's not
    strictly required to use it.

--- build.rs
+++ build.rs
@@ -1,5 +1,23 @@
 use std::io::Result;
+use walkdir::WalkDir;
+
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["./src/proto/test.proto"], &["./src/proto/"])?;
+    let proto_root = "./src/proto";
+
+    // Recursively collect all .proto files in the proto_root directory
+    let protos: Vec<_> = WalkDir::new(proto_root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| {
+            e.path()
+                .extension()
+                .map(|ext| ext == "proto")
+                .unwrap_or(false)
+        })
+        .map(|e| e.path().to_owned())
+        .collect();
+
+    prost_build::compile_protos(&protos, &[proto_root])?;
+
     Ok(())
 }
