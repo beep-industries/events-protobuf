8a2f3047c04b285a3dff4c29d697f9ff2c070dac
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-11-15 15:33:40 +0100

    feat(lib): remove tests
    The Rust lib had a test to showcase the use of the protobuf structs.
    However, these tests serve no other purpose, Protobuf validation is done
    at compile time (cargo build).
    
    As such they can be removed.
    
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,3 @@ pub mod test_svc_merchandise {
     include!(concat!(env!("OUT_DIR"), "/test_svc.merchandise.rs"));
 } // This module (and the generated structs) are defined here to be part of the lib's public API
 
-#[cfg(test)]
-mod tests {
-    use crate::test_svc_merchandise;
-
-    #[test]
-    fn compile_test_proto() {
-        let shirt1 = test_svc_merchandise::Shirt {
-            ..Default::default()
-        };
-
-        assert_eq!(shirt1.graphics, None);
-    }
-}
